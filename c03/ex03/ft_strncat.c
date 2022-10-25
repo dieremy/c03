7e023381ee541f2f43be689d4701ad2a730c0a61
Will's Lullaby <100817004+dieremy@users.noreply.github.com>
2022-10-25 23:29:01 +0200

    Add files via upload

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	dest_size;
+
+	i = 0;
+	dest_size = 0;
+	while (dest[dest_size])
+		dest_size++;
+	while (i < nb && src[i])
+	{
+		dest[dest_size] = src[i];
+		dest_size++;
+		i++;
+	}
+	dest[dest_size] = '\0';
+	return (dest);
+}
